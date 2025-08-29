0ca40358e2d45c8227b7d38e5db22c60ebe812ec
Rohit kanwar <kanwar.rohit2905@gmail.com>
2025-08-29 23:33:02 +0530

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use("/api", router);
 app.get("/test", (req,res) => {
   return res.send("Server is running...")
 })
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
